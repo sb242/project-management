ccc8d29e4ca2e01c775bda01d4c96e5fa6a50fbc
Spencer <spencerb242@proton.me>
2023-01-11 13:22:22 -0700

    setup firebase config file

---
+++ firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCI9XZ815JQ4N6trE4B7GN4vAJ1lxTq5Ig",
+  authDomain: "projectmanagement-2a8af.firebaseapp.com",
+  projectId: "projectmanagement-2a8af",
+  storageBucket: "projectmanagement-2a8af.appspot.com",
+  messagingSenderId: "250772108442",
+  appId: "1:250772108442:web:03263e5eb284b70db8db23",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
