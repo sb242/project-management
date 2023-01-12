cae1b80d04c07c5b100c1ead5f49957ffb6c8e4e
Spencer <spencerb242@proton.me>
2023-01-12 13:41:01 -0700

    modified useSignup hook to handle a user uploading an image and store it in firebase storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCI9XZ815JQ4N6trE4B7GN4vAJ1lxTq5Ig",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig);
 //init services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 //timestamp
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, projectStorage, timestamp };
