c4d4960b9970cb8f0fb137cdb01214f61c48c69b
Spencer <spencerb242@proton.me>
2023-01-12 16:13:31 -0700

    created a sidebar component for the right side which shows all of the users and if their online

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Create from "./pages/create/Create";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import OnlineUsers from "./components/OnlineUsers";
 
 function App() {
   const { user, authIsReady } = useAuthContext();
@@ -46,6 +47,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
