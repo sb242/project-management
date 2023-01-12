f8f3cba64c6727337d0b38b5b3c85e98b5044734
Spencer <spencerb242@proton.me>
2023-01-12 10:04:03 -0700

    addred react-router-dom as dependency and then setup routing for our pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+//styles
+import "./App.css";
+
+//pages and components
+import Login from "./pages/login/Login";
+import Project from "./pages/project/Project";
+import Signup from "./pages/signup/Signup";
+import Create from "./pages/create/Create";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/project/:id">
+              <Project />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
