25fdc2aa8e617832b0ed5d1a6582b4464c9c87fe
Spencer <spencerb242@proton.me>
2023-01-13 13:20:06 -0700

    fleshed out dashboard page to show current projects

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,6 +1,17 @@
+import ProjectList from "../../components/ProjectList";
+import { useCollection } from "../../hooks/useCollection";
+
 //styles
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
