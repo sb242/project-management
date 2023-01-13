adf5a2b4eb60b9f3b1f9856fb1b5f49676ca9dff
Spencer <spencerb242@proton.me>
2023-01-13 10:41:54 -0700

    created form inputs for creating a new project as well and showing when a user is online

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,62 @@
+import { useState } from "react";
+
 //styles
 import "./Create.css";
 
 export default function Create() {
-  return <div>Create</div>;
+  //form field values
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate);
+  };
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            type="text"
+            required
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            type="date"
+            required
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          {/* category select here */}
+        </label>
+        <label>
+          <span>Assign to:</span>
+          {/* assignee select here */}
+        </label>
+        <button className="btn">Add Project</button>
+      </form>
+    </div>
+  );
 }
