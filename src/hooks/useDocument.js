1783f341d0d584dadbeaba8ac8618341e52cb0cc
Spencer <spencerb242@proton.me>
2023-01-13 13:36:38 -0700

    created custom hook useDocument to handle getting specific documents from firestore based on collection and known id

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { projectFirestore } from "../firebase/config";
+
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+
+  //realtime data for document
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id);
+    const unsub = ref.onSnapshot(
+      (snapshot) => {
+        setDocument({ ...snapshot.data(), id: snapshot.id });
+        setError(null);
+      },
+      (err) => {
+        console.log(err.message);
+        setError("failed to get document");
+      }
+    );
+    return () => unsub();
+  }, [collection, id]);
+
+  return { document, error };
+};
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,6 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+
 //styles
 import "./Project.css";
 
 export default function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="project-details">
+      <h1></h1>
+    </div>
+  );
 }
