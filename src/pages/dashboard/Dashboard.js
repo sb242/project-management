b5bf2661bb1d47b8b07340f20a74a0a8e2a15557
Spencer <spencerb242@proton.me>
2023-01-15 19:44:37 -0700

    started a projectFilter component which now outputs a list of filter buttons a user can click on

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import ProjectList from "../../components/ProjectList";
+import ProjectFilter from "./ProjectFilter";
 import { useCollection } from "../../hooks/useCollection";
 
 //styles
@@ -11,6 +12,7 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   );
