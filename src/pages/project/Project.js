6e8a7bfe1d34fbf7255eb45922149619115e678a
Spencer <spencerb242@proton.me>
2023-01-15 18:51:32 -0700

    updated useFirestore hook to handle being able to update documents

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -4,6 +4,7 @@ import ProjectSummary from "./ProjectSummary";
 
 //styles
 import "./Project.css";
+import ProjectComments from "./ProjectComments";
 
 export default function Project() {
   const { id } = useParams();
@@ -19,6 +20,7 @@ export default function Project() {
   return (
     <div className="project-details">
       <ProjectSummary project={document} />
+      <ProjectComments />
     </div>
   );
 }
